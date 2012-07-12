3d30c2e55bae9637133ba50123b6bd93aadfc285
Ngo Minh Nam <emoinrp@gmail.com>
2012-07-12 22:25:32 +1000

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='python-gcm',
+    version='0.1',
+    packages=['gcm',],
+    license='MIT',
+    author='Minh Nam Ngo',
+    author_email='nam@namis.me',
+    url='http://blog.namis.me/python-gcm/',
+    description='Python client for Google Cloud Messaging for Android (GCM)',
+    long_description=open('README.md').read(),
+)
