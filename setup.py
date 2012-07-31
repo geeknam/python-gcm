9d5a79ba1d5a953f103b3e72dbb1a7a54b0a9c8d
Ngo Minh Nam <emoinrp@gmail.com>
2012-08-01 00:17:10 +1000

    Added MANIFEST file. Added py_modules + keywords in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 
 setup(
     name='python-gcm',
-    version='0.1.2',
-    packages=['gcm',],
+    version='0.1.3',
+    packages=['gcm'],
+    py_modules=['gcm'],
     license='MIT',
     author='Minh Nam Ngo',
     author_email='nam@namis.me',
     url='http://blog.namis.me/python-gcm/',
     description='Python client for Google Cloud Messaging for Android (GCM)',
     long_description=open('README.md').read(),
+    keywords='android gcm push notification google cloud messaging',
 )
