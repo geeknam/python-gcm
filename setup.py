517bdd7231c3f799ab413b824a2d0e87951409dd
Ngo Minh Nam <emoinrp@gmail.com>
2012-08-15 14:24:50 +1000

    Added LICENSE

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from distutils.core import setup
 
 setup(
     name='python-gcm',
-    version='0.1.3',
-    packages=['gcm'],
-    py_modules=['gcm'],
-    license='MIT',
+    version='0.1.2',
+    packages=['gcm',],
+    license=open('LICENSE').read(),
     author='Minh Nam Ngo',
     author_email='nam@namis.me',
     url='http://blog.namis.me/python-gcm/',
     description='Python client for Google Cloud Messaging for Android (GCM)',
     long_description=open('README.md').read(),
-    keywords='android gcm push notification google cloud messaging',
 )
